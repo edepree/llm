cd0b067114e9476548961d668cceec3776f23aec
edepree <edepree@users.noreply.github.com>
2026-06-06 15:20:27 +0000

    added additional validation and linting

--- validate.sh
+++ validate.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+uv run ansible-playbook --syntax-check deploy.yml
 uv run ansible-lint deploy.yml tasks/
+uv run yamllint --strict deploy.yml tasks/ templates/
